80c232f2023fa361b4d09da538ac711fd19c2327
Bimgor_Ayk <haykooo3@gmail.com>
2026-03-12 14:17:04 +0300

    add colorama

--- app/utils.py
+++ app/utils.py
@@ -1,13 +1,20 @@
-from time import strftime
-import datetime
-import shutil
-import os
 from pathlib import Path
+from colorama import init, Fore, Style
 
 PATH = (Path(__file__).parent / "../DATA").resolve()
 
 month_dict = {'01': 'Январь', '02': 'Февраль', '03': 'Март', '04': 'Апрель', '05': 'Май', '06': 'Июнь',
               '07': 'Июль', '08': 'Август', '09': 'Сентябрь', '10': 'Октябрь', '11': 'Ноябрь', '12': 'Декабрь'}
+
+init()
+
+
+def input_color(prompt, color=Fore.WHITE):
+    return input(f"{color}{prompt}{Style.RESET_ALL}")
+
+
+def print_color(text, color=Fore.WHITE):
+    print(f"{color}{text}{Style.RESET_ALL}")
 
 
 def open_month(file_name, month, mode="r"):
@@ -18,38 +25,41 @@ def open_month(file_name, month, mode="r"):
             if st0[2][3:5] == f"{month:0>2}" and st0[12] == '  .  ':
                 zakaz_sum = 0.0
                 for j in i[2:]:
-                    str_sum = 0.0
                     poz, dlina, visota, kol, pusto, vid = j.split("|")[0:6]
                     if poz == "R" or vid[0] in ("R", "К") or int(dlina) < 50 or int(visota) < 50:
                         continue
-                    str_sum = (int(dlina)*2+int(visota)*2)/1000*int(kol)
+                    str_sum = (int(dlina) * 2 + int(visota) * 2) / 1000 * int(kol)
                     zakaz_sum += str_sum
-                print(f"{st0[0]}: {zakaz_sum:.2f} м.п.")
+                print_color(f"{st0[0]}: {zakaz_sum:.2f} м.п.", Fore.YELLOW)
                 if st0[1].strip() == "Сердюченко":
                     month_sum_ser += zakaz_sum
                 else:
                     month_sum += zakaz_sum
-        print(f"Общий метраж Сердюченко, за {month_dict.get(f'{month:0>2}')} ({month:0>2}) : {month_sum_ser:.2f} м.п.")
-        print(f"Общий метраж Мы, за {month_dict.get(f'{month:0>2}')} ({month:0>2}) : {month_sum:.2f} м.п.")
-        print(f"Итого за {month_dict.get(f'{month:0>2}')} ({month:0>2}): {(month_sum + month_sum_ser):.2f} м.п.")
+        print_color(
+            f"Общий метраж Сердюченко, за {month_dict.get(f'{month:0>2}')} ({month:0>2}) : {month_sum_ser:.2f} м.п.",
+            Fore.GREEN)
+        print_color(f"Общий метраж Мы, за {month_dict.get(f'{month:0>2}')} ({month:0>2}) : {month_sum:.2f} м.п.",
+                    Fore.GREEN)
+        print_color(f"Итого за {month_dict.get(f'{month:0>2}')} ({month:0>2}): {(month_sum + month_sum_ser):.2f} м.п.",
+                    Fore.RED)
 
-        
+
 def open_order(file_name, order_number, mode="r"):
     with open(PATH / file_name, mode) as f:
         for i in [i.strip().split('\n') for i in f.read().split('#@#')[1:]]:
             st0, st1 = i[0].split('|'), i[1].split('|')
             if st0[0] == order_number and st0[12] == '  .  ':
-                print(st0[0])
+                print_color(f"Заказ %: {st0[0]}", Fore.MAGENTA)
                 order_sum = 0.0
                 for j in i[2:]:
                     str_sum = 0.0
                     poz, dlina, visota, kol, pusto, vid = j.split("|")[0:6]
                     if poz == "R" or vid[0] in ("R", "К") or int(dlina) < 50 or int(visota) < 50:
                         continue
-                    str_sum = (int(dlina)*2+int(visota)*2)/1000*int(kol)
+                    str_sum = (int(dlina) * 2 + int(visota) * 2) / 1000 * int(kol)
                     order_sum += str_sum
-                    print(f"{poz:>2}|{dlina:>4}|{visota:>4}|{kol:>2}|{vid:>3}| {str_sum} м.п.")
-                print(f"Общий метраж: {order_sum:.2f} м.п.")
+                    print_color(f"{poz:>2}|{dlina:>4}|{visota:>4}|{kol:>2}|{vid:>3}| {str_sum} м.п.", Fore.YELLOW)
+                print_color(f"Общий метраж: {order_sum:.2f} м.п.", Fore.GREEN)
                 break
         else:
             print("Заказ не найден")
--- run.py
+++ run.py
@@ -1,24 +1,28 @@
-from app.utils import open_month, open_order
+from app.utils import open_month, open_order, input_color, print_color
 import shutil
 from config import PATH_FILE
 from datetime import datetime
+from colorama import init, Fore, Style
 
 now = datetime.now()
+
 
 def main():
     shutil.copyfile(f"{PATH_FILE}\\Заказы{now:%y}.RSB", "DATA\\22.txt")
     while True:
-        my_input = input('Для выборо месяца введите 1:\nДля выбора заказа введите 2:\nДля выхода введите 0: ')
+        my_input = input_color('Для выборо месяца введите 1:\nДля выбора заказа введите 2:\nДля выхода введите 0: ',
+                               Fore.CYAN)
         if my_input == '1':
-            input_month = input("Введите месяц (в формате 1, 2, ..., 12): ")
+            input_month = input_color("Введите месяц (в формате 1, 2, ..., 12): ", Fore.CYAN)
             open_month("22.txt", input_month)
         elif my_input == '2':
-            input_order = input("Введите номер заказа: ")
+            input_order = input_color("Введите номер заказа: ", Fore.CYAN)
             open_order("22.txt", input_order)
         elif my_input == '0':
-            print("Выход из программы")
+            print_color("Выход из программы", Fore.CYAN)
             exit()
         else:
-            print("Неверный ввод")
+            print_color("Неверный ввод", Fore.CYAN)
+
 
 main()
